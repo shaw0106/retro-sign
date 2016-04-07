4ded0b197f9f9c4752591cd9e40b2b577a505cc2
Shannon <shaw0106@algonquinlive.com>
2016-04-07 08:03:31 -0400

    started css

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 var $svg = $('svg');
+
+var $svg = $('svg');
+
+$('.btn').on('click', function () {
+  $('svg').addClass('lineEngage');
+  $('svg').removeClass('lines');
+  $('.engage').addClass('engaged');
+});
